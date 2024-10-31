00d06bc30faa23d42736d70465b852e91f3d6390
Wubeshet Yimam <68907395+wubeshetA@users.noreply.github.com>
2024-10-31 13:33:39 +0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -6,5 +6,5 @@ pip3 install --upgrade pip
 pip3 install -r requirements.txt
 
 # make migrations
-python3.11 manage.py migrate 
-python3.11 manage.py collectstatic
+python3 manage.py migrate 
+python3 manage.py collectstatic
